04c9f087162e437e5ed4caf80e1c5d8b79a9b231
Grathium <grath@duck.com>
2022-09-26 00:22:46 +1000

    first working MVP of function conversion

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-mod read_file;
+mod util;
 mod functions;
 
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_contents: String = read_file::read_from_file(&args[1]);
+    let files: Vec<String> = env::args().collect();
+    let file_contents: String = util::read_from_file(&files[1]);
 
-    let mut file_processing = functions::replace_functions(file_contents);
+    let mut file_processing_out = functions::replace_functions(file_contents);
 
-    println!("{}", file_processing);
+    println!("{}", file_processing_out);
+
+    util::write_to_file(
+        &(files[1]).replace(".pyc", ".py"),
+        &file_processing_out
+    )
 }
--- src/read_file.rs
+++
@@ -1,8 +0,0 @@
-use std::fs;
-
-pub fn read_from_file(file_path: &String) -> String {
-    let contents: String = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-    return contents;
-}
